1d7cf92682974a592e2d5307dc945dbc11c848f4
Yvette Salinas <you@example.com>
2022-05-18 22:40:08 -0500

    variables and beginning of initial function

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,27 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Reminder: # refers to the id from the HTML
+//Global variables
+var passwordEl=document.querySelector("#password");
+var generatebtnEl = document.querySelector("#generate");
+var copybtnEl=document.querySelector()
+
+// Add event listener to generate button
+generatebtnEl.addEventListener("click",  function() {
+  var characterLength=prompt("Number of characters (choose between 8 and 25 characters):")
+  
+  if ((characterLength >= 8) && (characterLength >= 25)) {
+      let symbols = "~`! @#$%^&*()_-+={[}]|\:;'<,>.?/";
+      let numbers = "0123456789";
+      let lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+      let upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  }
+})
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
